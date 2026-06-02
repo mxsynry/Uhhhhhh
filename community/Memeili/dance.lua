1389eefe0ae16018b55ee697e27e9bf05c14a883
testing734 <aweser174@gmail.com>
2026-06-02 12:33:02 +0530

    add community dance (#12)
    * Create dance.lua
    
    * :3
    
    * Update dance.lua
    
    * Update dance.lua
    
    * Update list.txt
    
    * Update list.txt

---
+++ community/Memeili/dance.lua
@@ -0,0 +1,32 @@
+local modules = {}
+table.insert(modules, function()
+    local m = {}
+    m.ModuleType  = "DANCE"
+    m.Name        = "Miss Me (Hello Neighbor Mix)"
+    m.Description = "Music Goes hard asf"
+    m.Assets = {"MissMeFix.anim@https://github.com/STEVE-916-create/Uhhhhhh/raw/refs/heads/main/community/Memeili/MissMeFix.anim", "TungBung.mp3@https://github.com/STEVE-916-create/Uhhhhhh/raw/refs/heads/main/community/Memeili/TungBung.mp3"}
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+    local animator = nil
+    local start    = 0
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("TungBung.mp3"),"TungBung", 0.8, NumberRange.new(0, 45.5))
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("MissMeFix.anim"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+    m.Destroy = function(figure)
+        animator = nil
+    end
+    return m
+end)
+return modules
