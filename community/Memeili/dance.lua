985e285f5bc3bbac3c4948dcdde6ab8d8e259242
STEVE <77476461+STEVE-916-create@users.noreply.github.com>
2026-06-04 15:52:14 +0800

    fix these things

--- community/Memeili/dance.lua
+++ community/Memeili/dance.lua
@@ -4,7 +4,7 @@ table.insert(modules, function()
     m.ModuleType  = "DANCE"
     m.Name        = "Miss Me (Hello Neighbor Mix)"
     m.Description = "Music Goes hard asf"
-    m.Assets = {"MissMeFix.anim@https://github.com/STEVE-916-create/Uhhhhhh/raw/refs/heads/main/community/Memeili/MissMeFix.anim", "TungBung.mp3@https://github.com/STEVE-916-create/Uhhhhhh/raw/refs/heads/main/community/Memeili/TungBung.mp3"}
+    m.Assets = {"MissMeFix.anim@MARKET/Memeili/MissMeFix.anim", "TungBung.mp3@MARKET/Memeili/TungBung.mp3"}
     m.Config = function(parent)
         Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
     end
@@ -35,7 +35,7 @@ table.insert(modules, function()
     m.ModuleType  = "DANCE"
     m.Name        = "Real one knows this isn't avgn😂✌"
     m.Description = "SON NO COPYING 😂😂😂"
-    m.Assets = {"KwikFlip.animhttps://github.com/STEVE-916-create/Uhhhhhh/raw/refs/heads/main/community/Memeili/KwikFlip.anim", "Kwik Flip.mp3https://github.com/STEVE-916-create/Uhhhhhh/raw/refs/heads/main/community/Memeili/Kwik%20Flip.mp3"}
+    m.Assets = {"KwikFlip.anim@MARKET/Memeili/KwikFlip.anim", "Kwik Flip.mp3@MARKET/Memeili/Kwik%20Flip.mp3"}
     m.Config = function(parent)
         Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
     end
